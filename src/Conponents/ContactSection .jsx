a6a33efe6eff50c3fa4017724bf2cb0cdc93ad66
Jubar Alam <jubaralam11022@gmail.com>
2024-06-17 11:39:04 +0530

    bugs have fixed releted to css heroSection

--- src/Conponents/ContactSection .jsx
+++ src/Conponents/ContactSection .jsx
@@ -14,7 +14,7 @@ const handleSubmit = (e)=>{
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ">
         <h2 className="text-3xl font-bold text-center mb-8">Contact Us</h2>
         <div className=" flex justify-around flex-wrap-reverse	 items-center">
-          <div className="p-4 sm:w-full md:w-full">
+          <div className="p-4  md:w-full  sm:w-full">
             <h3 className="text-xl font-semibold mb-4">Leave us a message</h3>
             <form>
               <div className="mb-4  ">
