593b81fdeb77ee261ae9e5ca3d20381f4360fdb2
Jubar Alam <jubaralam11022@gmail.com>
2024-06-17 11:51:40 +0530

    bugs have fixed releted to css heroSection

--- src/Conponents/ContactSection .jsx
+++ src/Conponents/ContactSection .jsx
@@ -3,18 +3,16 @@
 import React from "react";
 import poster from "../assets/direction.png";
 const ContactSection = () => {
-
-const handleSubmit = (e)=>{
-
- alert(  e.target)
-}
+  const handleSubmit = (e) => {
+    alert(e.target);
+  };
 
   return (
     <section id="ContactSection" className="bg-gray-200 py-12">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ">
         <h2 className="text-3xl font-bold text-center mb-8">Contact Us</h2>
         <div className=" flex justify-around flex-wrap-reverse	 items-center">
-          <div className="p-4  md:w-full  sm:w-full">
+          <div className=" p-4 w-[500px]">
             <h3 className="text-xl font-semibold mb-4">Leave us a message</h3>
             <form>
               <div className="mb-4  ">
@@ -51,7 +49,7 @@ const handleSubmit = (e)=>{
                 ></textarea>
               </div>
               <button
-              onClick={handleSubmit}
+                onClick={handleSubmit}
                 type="submit"
                 className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 w-full"
               >
@@ -59,7 +57,7 @@ const handleSubmit = (e)=>{
               </button>
             </form>
           </div>
-          <div className="p-4">
+          <div className="p-4 w-[500px]">
             <h3 className="text-xl font-semibold mb-4">Reach out to us</h3>
             <p className="text-gray-700 mb-2">
               <strong>Email:</strong> info@example.com
@@ -73,18 +71,19 @@ const handleSubmit = (e)=>{
             </p>
             <div className=" mb-2 bg-orange-500  h-[45px] hover:bg-orange-600 text-white font-bold py-2 px-3  rounded flex  justify-between align-middle">
               <div>
-
-              <a
-                href="https://maps.app.goo.gl/nE5xxvG3mDJiVeCMA"
-                target="_blank"
+                <a
+                  href="https://maps.app.goo.gl/nE5xxvG3mDJiVeCMA"
+                  target="_blank"
                 >
-                <strong>Direction:</strong> Google Map
-              </a>
-        
-                </div>
+                  <strong>Direction:</strong> Google Map
+                </a>
+              </div>
               <div className="h-full align-middle">
-
-              <img src={poster} alt="" className=" w-10 sm:-w-6 block ml-2 h-full " />
+                <img
+                  src={poster}
+                  alt=""
+                  className=" w-10 sm:-w-6 block ml-2 h-full "
+                />
               </div>
             </div>
           </div>
